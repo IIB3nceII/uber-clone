907c178df187732da9660cc7e14dec00dc202573
Bence Papp <bencepapp1101@gmail.com>
2023-01-06 16:11:19 +0100

    🚀 init

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Bence Papp on 2023. 01. 06..
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
